eb19882f9786b20bd98f9c78c7e46d42bfb3bff5
MrCurls <JonCurls@gmail.com>
2022-06-06 14:59:56 -0500

    server setup

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: "Would you kindly enter a username.",
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: "Would you kindly enter an email address.",
+      unique: true,
+      match: [/.+@.+\..+/],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thoughts",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// creates the user model using the UserSchema
+const User = model("User", UserSchema);
+
+// virtual to count the friends array
+UserSchema.virtual("friendCount").get(function () {
+  return;
+});
